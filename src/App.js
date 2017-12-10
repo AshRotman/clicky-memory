7f253b4ecaeb66fb12e77ed88c9dad59a1c5e0fd
Ash Rotman <quigibow@gmail.com>
2017-12-09 23:19:26 -0700

    initial

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React, { Component } from "react";
+import FriendCard from "./components/Cards";
+import Wrapper from "./components/Wrapper";
+import Title from "./components/Title";
+import friends from "./friends.json";
+import "./App.css";
+
+class App extends Component {
+  // Setting this.state.friends to the friends json array
+  state = {
+    friends
+  };
+
+  render() {
+    return (
+      <Wrapper>
+        <Title>Friends List</Title>
+        {this.state.friends.map(friend => (
+          <FriendCard
+            id={friend.id}
+            key={friend.id}
+            name={friend.name}
+            image={friend.image}
+          />
+        ))}
+      </Wrapper>
+    );
+  }
+}
+
+export default App;
